2b8d3263a73a7f83c6b3a4e0bc99a95ac83134d9
Entra21 <entra21@proway.treina>
2020-07-06 22:13:29 -0300

    Aprendendo os tipos básicos.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace aula_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é o seu nome?");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Olá, {userName}");
+
+            var myChar = 'a'; 
+            var myText = "Texto"; 
+            var myNumber = 10; 
+            var myBoolean = false;
+            var myFloat = 0.99;
+                     
+            Console.WriteLine("Você é maior de idade?");
+            var result = Console.ReadLine();
+            var myBoolean1 = result == "sim";
+            Console.WriteLine(myBoolean1);
+        }
+    }
+}
